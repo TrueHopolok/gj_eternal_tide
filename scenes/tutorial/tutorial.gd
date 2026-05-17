80a739d7e740e4061e8371bd74b283b68114b9f3
TrueHopolok <vadim.truhanov@inbox.lv>
2026-05-17 21:32:40 +0300

    added full tutorial

--- scenes/tutorial/tutorial.gd
+++ scenes/tutorial/tutorial.gd
@@ -4,7 +4,7 @@ extends CanvasLayer
 
 @export var hints: Dictionary[int, String]
 
-@onready var label: Label = $Hint
+@onready var _label: RichTextLabel = $Hint
 
 
 func on_level_switched(level_num: int) -> void:
@@ -13,4 +13,14 @@ func on_level_switched(level_num: int) -> void:
 
 
 func _show_hint(hint: String) -> void:
-	pass
+	if not is_node_ready():
+		await ready
+
+	var l := _label.duplicate()
+	l.show()
+	l.text = hint
+	add_child(l)
+	var t := l.create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	t.tween_property(l, "position:y", -20, 3.0).as_relative()
+	t.parallel().tween_property(l, "modulate:a", 0.0, 10.0).set_ease(Tween.EASE_IN_OUT)
+	t.chain().tween_callback(l.queue_free)
